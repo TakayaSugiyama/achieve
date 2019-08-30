56d41e28b9cb21c0993658c16863fd9c4d588317
杉山貴哉 <s.takaya1027@gmail.com>
2019-08-30 16:04:40 +0900

    ヘッダーを微調整

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
   def current_user 
-    @user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def login?
@@ -8,5 +8,5 @@ module SessionsHelper
       true
     end
   end
-  
+
 end
