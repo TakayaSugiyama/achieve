a479f10bb4a7c2d75138d819593471c798c7d9dd
杉山貴哉 <s.takaya1027@gmail.com>
2019-08-30 17:03:10 +0900

    投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  def new 
+    @post = Picture.new(flash[:post])
+  end
+
+  def create 
+    @post = current_user.pictures.build(picture_params)
+    #binding.pry
+    if @post.save 
+      redirect_to posts_path, notice: "投稿しました。"
+    else   
+      redirect_to new_post_path, flash: {post: @post, error_messages: @post.errors.full_messages}
+    end
+  end
+
+  private 
+
+  def picture_params 
+    params.require(:picture).permit(:description,:image)
+  end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :image , presence: true 
+  validates :description, length: {maximum: 50},
+                          presence: true
 end
