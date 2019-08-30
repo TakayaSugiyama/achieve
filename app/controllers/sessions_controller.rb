2496821be7c4d66eea172f2dbf5a580ea65aad1c
杉山貴哉 <s.takaya1027@gmail.com>
2019-08-30 15:04:26 +0900

    ログイン機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      redirect_to @user, notice: "ログインに成功しました。"
+    else 
+      flash.now[:notice] = "ログインに失敗しました。"
+      render "sessions/new"
+    end
   end
 
-  def destroy
-  end
+  def destroy;end
 
-  def new
-  end
+  def new;end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'sessions/new'
+  get "login" , to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "login", to: "sessions#destroy"
   resources :users, except: [:index,:new]
   get "signup" ,to: "users#new"
   root "home#index"
