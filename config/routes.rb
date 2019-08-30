6f4f0caff9ad51ec1b8d0af07f53b94770c6094e
杉山貴哉 <s.takaya1027@gmail.com>
2019-08-30 14:32:21 +0900

    Sessionsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'sessions/new'
   resources :users, except: [:index,:new]
   get "signup" ,to: "users#new"
   root "home#index"
